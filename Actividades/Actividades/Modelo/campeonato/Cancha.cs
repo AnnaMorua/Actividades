6bd755d944f1f5bbfd704ce4b58adab2b65d6b64
LAB02-06 <LAB02-06@DESKTOP-7KA7BRS>
2019-03-07 13:14:34 -0600

    actividades en clase

---
+++ Actividades/Actividades/Modelo/campeonato/Cancha.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividades.Modelo.campeonato
+{
+    class Cancha
+    {
+        private int _id;
+        private string _nombre;
+        private string _ubicacion;
+
+        public int Id
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    this._id = value;
+                }
+            }
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return this._nombre;
+            }
+            set
+            {
+                if (value != null || (value.Length > 3 && value.Length <= 300))
+                {
+                    this._nombre = value;
+                }
+            }
+
+        }
+    }
+}
