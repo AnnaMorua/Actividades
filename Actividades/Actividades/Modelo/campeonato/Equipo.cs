269b25bdd5f535c91a294c179838b107bb4e399a
LAB02-06 <LAB02-06@DESKTOP-7KA7BRS>
2019-03-07 14:03:10 -0600

    clase de hoy

---
+++ Actividades/Actividades/Modelo/campeonato/Equipo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividades.Modelo.campeonato
+{
+    class Equipo
+    {
+        private int _id;
+        private string _nombre;
+        private string _comunidad;
+        private string _entrenador;
+
+
+        //para que nombre siempre asigne un valor
+        public Equipo()
+        {
+            this._nombre = "Deportivo Rio Laja";
+        }
+        public int Id {
+            get
+            {
+                return this._id;
+            }
+            set
+            {
+                if (value > 0)
+                    this._id = value;
+            }
+        }
+
+
+        public string Nombre { get; set; }
+        public string Comunidad { get; set; }
+        public string Entrenador { get; set; }
+
+    }
+}
---
+++ Actividades/Actividades/Modelo/campeonato/Jugador.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividades.Modelo.campeonato
+{
+    class Jugador
+    {
+        private int _id;
+        private string _nombre;
+        private string _apellidos;
+        private DateTime _fechaNacimiento;
+        private byte _numero;
+        private Equipo _idEquipo;
+        public Jugador()
+        {
+            this._nombre = "Papulince";
+            this._apellidos = "Del campo de la via lactea";
+            this._fechaNacimiento = DateTime.Today;
+        }
+
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public byte Numero { get; set; }
+        public Equipo IdEquipo
+        {
+            get
+            {
+                return this._idEquipo;
+            }
+            set
+            {
+                if(value != null || ((Equipo)value).Nombre.ToLower().Equals("llanito"))
+                {
+                    this._idEquipo = value;
+                }
+            }
+        }
+
+    }
+}
